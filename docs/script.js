8de10e85a204fae850872c40da5cd685d1e632b9
cocoapc911 <kokoapc0911@gmail.com>
2022-06-12 22:22:22 +0900

    commit

---
+++ docs/script.js
@@ -0,0 +1,54 @@
+let element = document.getElementById('mytext');
+let ints = document.getElementById('content');
+
+setUserAgent(
+    document.querySelector('iframe').contentWindow, 
+    'Aakash Chakravarthy Mobile Agent'
+);
+
+function btn1Click(){
+  console.log("https://www.youtube.com/watch?v=" + element.value);
+document.getElementById("ifr").src="https://www.youtube.com/embed/" + element.value;
+  if (window.navigator.userAgent != userAgent) {
+    var userAgentProp = {
+      get: function() {
+        return userAgent;
+      }
+    };
+    try {
+      Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+    } catch (e) {
+      window.navigator = Object.create(navigator, {
+        userAgent: userAgentProp
+      });
+    }
+  }
+}
+
+
+function clicks(){
+  console.log(element.value);
+　document.getElementById("ifr2").src=ints.value;
+  if (window.navigator.userAgent != userAgent) {
+    var userAgentProp = {
+      get: function() {
+        return userAgent;
+      }
+    };
+    try {
+      Object.defineProperty(window.navigator, 'userAgent', userAgentProp);
+    } catch (e) {
+      window.navigator = Object.create(navigator, {
+        userAgent: userAgentProp
+      });
+    }
+  }
+
+}
+
+function cc(){
+  console.log(element.value);
+　window.open('https://math.005net.com/youten.php')
+
+}
+
